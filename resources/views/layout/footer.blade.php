f5b78ed81a60d3c4ed34cd3a4f24cf0e924201a5
IRADUNDA Blaise Antonio <antonio.iradukunda@gmail.com>
2015-07-09 20:44:33 +0200

    adding app.css and app.js and jquery library

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,6 @@
+<!-- Javascript files -->
+    <script type="text/javascript" src="{{ url('js/vendor/jquery.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/vendor/bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ url('js/app.js') }}"></script>
+    </body>
+</html>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,5 @@
+@include('layout.header')
+    <!-- Javascript files -->
+    <script type="text/javascript" src="{{ url('js/vendor/bootstrap.min.js') }}"></script>
+    </body>
+</html>
