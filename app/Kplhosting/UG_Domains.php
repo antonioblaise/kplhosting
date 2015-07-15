403aba1d2ef72330a0394d6642d83db29d548937
IRADUNDA Blaise Antonio <antonio.iradukunda@gmail.com>
2015-07-15 15:03:08 +0200

    Testing Changes 3

--- app/Kplhosting/UG_Domains.php
+++ app/Kplhosting/UG_Domains.php
@@ -43,7 +43,7 @@
 
 		public static function sendRequest($xmlData){
 			$client = new Client();
-			$request = $client->get('https://new.registry.co.ug:8006/api', ['body' => $xmlData , 'verify' => false, 'timeout' => 8]);
+			$request = $client->get('https://new.registry.co.ug:8006/api', ['body' => $xmlData , 'verify' => false, 'timeout' => 0]);
 			return $request;
 		}
 	}
