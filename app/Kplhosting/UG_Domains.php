c477bf5547f0cac90813fac8bc59a2bfd115275a
IRADUNDA Blaise Antonio <antonio.iradukunda@gmail.com>
2015-07-16 21:06:07 +0200

    Adding Guzzle HTTP Client

--- app/Kplhosting/UG_Domains.php
+++ app/Kplhosting/UG_Domains.php
@@ -10,26 +10,12 @@
 
 		private $domain = null;
 		private $extensions = null;
-		private $xmlQuery;
-		private static $client;
+		private $client;
 
 		public function __construct(){
-			$this->extensions = $this->ug_extensions;
 		}
 
 		public static function isAvailable($domain){
-			$xml = "
-				<epp xmlns=\"urn:ietf:params:xml:ns:epp-1.0\">
-					<command>
-				        <info>
-				            <domain:info xmlns:domain=\"http://epp.cfi.co.ug/doc\">
-				                <domain:name>$domain</domain:name>
-				            </domain:info>
-				        </info>
-				    </command>
-				</epp>";
-			$response = self::sendRequest($xml);
-			return $response;
 		}
 		public function create($domain){
 			
@@ -45,22 +31,7 @@
 
 		public function whois(){}
 
-		public function constructQuery($xml){
-			$this->xmlQuery = $final_xml;
-			return $final_xml;
-		}
-
-		public static function sendRequest($xmlData){
-			$client = new HTTP_Client([
-				'url' => 'https://new.registry.co.ug:8006/api'
-			]);
-
-			$options = [
-				'params' => $xmlData 
-			]; 
-
-			$response = $client->post('https://new.registry.co.ug:8006/api', $options);
-			//$response = $client->connect_socket($xmlData);
-			return $response;
+		public function sendRequest($xmlData){
+			
 		}
 	}
